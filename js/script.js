592830aaf0462a2ebedafdef2224d9201bc8a5da
JeniferQ <144718154+JeniferQ@users.noreply.github.com>
2024-11-20 14:37:19 -0500

    Add scroll animation
    In this commit, the earbud canvas sequence was added and animated with the GSAP scrollTrigger plugin.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 (() => {
-  hotspots = document.querySelectorAll(".Hotspot");
+  const canvas = document.querySelector(".sequence");
+  const context = canvas.getContext("2d");
+
+  canvas.width = 1200;
+  canvas.height = 1080;
+
+  const frameCount = 161;
+  const images = [];
+  const sequence = { frame: 0 };
+
+  for(let i = 0; i < frameCount; i++){
+      const img = new Image();
+      img.src =`images/sequence/website${(i + 1).toString().padStart(3, '0')}.webp`;
+      images.push(img);
+  }
+
+  gsap.to(sequence, {
+      frame: 159,
+      snap: "frame",
+      scrollTrigger: { trigger: ".sequence", pin: true, scrub: 2, start: "top top" }, onUpdate: render
+  });
+  
+  images[0].addEventListener("load", render);
+
+  function render(){
+      context.clearRect(0,0, canvas.width, canvas.height);
+      console.log(images[sequence.frame]);
+      context.drawImage(images[sequence.frame], 0, 0);
+  }
+})();
+
+(() => {
+  const hotspots = document.querySelectorAll(".Hotspot");
 
   const infoBoxes = [
     { title: "Noise Cancellation", text: "Premium rubber tips with a sealed fit that cancels all background noise.", img: "images/poster.png" },
@@ -13,12 +45,17 @@
       let card = document.querySelector(`#mobile-card-${index+1}`);
       let hotspot = document.querySelector(`#hotspot-${index+1}`);
 
-        card.innerHTML = `<div class="number">${index+1}</div>
-        <h2>${infoBoxes[index].title}</h2>
-        <p>${infoBoxes[index].text}</p>
-        <img src="${infoBoxes[index].img}"></img>`
+        card.innerHTML = `<div class="shape">
+        <div class="hexagon"><p>${index+1}</p></div>
+        <div class="side-line"></div></div>
 
-        hotspot.innerHTML = `<h2 class="appear">${infoBoxes[index].title}</h2>
+        <div class="info">
+        <h3 class="title">${infoBoxes[index].title}</h3>
+        <p>${infoBoxes[index].text}</p>
+        <img src="${infoBoxes[index].img}"></img>
+        </div>`
+
+        hotspot.innerHTML = `<h2 class="appear title">${infoBoxes[index].title}</h2>
         <p class="appear">${infoBoxes[index].text}</p>
         <img class="appear" src="${infoBoxes[index].img}"></img>`
     });
@@ -45,3 +82,14 @@
 
 })();
 
+(() => {
+  const divisor = document.querySelector(".xray");
+  const slider = document.querySelector(".inputRange")
+
+function moveDivisor() {
+    console.log(slider.value);
+    divisor.style.width = `${slider.value}px`;
+}
+
+  slider.addEventListener("input", moveDivisor);
+})();
